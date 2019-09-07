5915ed2d9e4d762762dc042fd421349591abeaac
BobbaTea <bobba.akash@gmail.com>
2019-09-07 11:04:22 -0400

    skrrrrt

--- safetynetserver/app/database.py
+++ safetynetserver/app/database.py
@@ -1,6 +1,24 @@
 from pymongo import MongoClient
+from bson.json_util import dumps
+from bson.objectid import ObjectId
 
-database = MongoClient()['safetynetdb']
+database = MongoClient('35.224.43.169', 27017)['safetynetdb']
 
 users = database['users']
 places = database['places']
+
+
+def add(report):
+    reports = database.reports
+    report_id = reports.insert_one(report).inserted_id
+    return report_id
+
+
+def retrieve(ulat, llat, ulong, llong):
+    data = dumps(database.reports.find({'latitude': {'$gt': llat}, 'latitude': {'$lt': ulat},  'longitude': {
+                 '$gt': llong}, 'longitude': {'$lt': ulong}}, {'email': 1, 'longitude': 1, 'latitude': 1, 'description': 1, 'tags': 1}))
+    return data
+
+
+def get_image(objectid):
+    return dumps(database.reports.find({'_id': ObjectId(objectid)}, {'image': 1}))
