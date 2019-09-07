4fed73833cf6a51e0c7f1cc90658f7e37d6b36cd
Rahul Shah <rahuldshah3@gmail.com>
2019-09-07 16:46:27 -0400

    UPDATE REQUIREMENTs.TXT (this mean u pip install -r req.txt)

---
+++ safetynetserver/app/send_message.py
@@ -0,0 +1,16 @@
+# dw if you get errors, i talked to Liz from Twilio and she was like it runs without problems so ignore it :clown:
+from twilio.rest import Client
+
+client = Client('ACafe80e4efd87b17c456eaf0b3616e6d7',
+                'c3c48f2b848b0f6e8483112c51fc969b')
+
+
+def send(target_number):
+    message = client.messages.create(
+        to=target_number,
+        from_='+17325611613',
+        body='An alert in your area was detected!')
+
+
+if __name__ == "__main__":
+    send('+16099006337')
