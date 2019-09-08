e28414e59f50a3549495d1081cdc4d6855aa7fca
Varun Ramani <varun.ramani@gmail.com>
2019-09-08 03:29:40 -0400

    markers

--- safetynetapp/lib/homescreen.dart
+++ safetynetapp/lib/homescreen.dart
@@ -3,7 +3,10 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flt_telephony_info/flt_telephony_info.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' as prefix0;
 import 'package:location/location.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -13,6 +16,7 @@ import 'globals.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_exif_rotation/flutter_exif_rotation.dart';
 import 'package:vibration/vibration.dart';
+
 class ActionButton extends StatelessWidget {
   VoidCallback onPress;
   Color color;
@@ -27,10 +31,10 @@ class ActionButton extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         Container(
-          padding: EdgeInsets.all(width * ((1 / 10) / 2)),
+          padding: EdgeInsets.all(width * ((1 / 12) / 2)),
           child: RawMaterialButton(
-            padding: EdgeInsets.all(width * ((1 / 9) / 2)),
-            child: Icon(iconData, size: width * (1 / 9), color: color),
+            padding: EdgeInsets.all(width * ((1 / 12) / 2)),
+            child: Icon(iconData, size: width * (1 / 12), color: color),
             onPressed: onPress,
             splashColor: color.withOpacity(0.5),
             shape: CircleBorder(),
@@ -41,6 +45,8 @@ class ActionButton extends StatelessWidget {
     );
   }
 }
+
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -53,9 +59,21 @@ class _HomeScreenState extends State<HomeScreen> {
       children: <Widget>[
         Row(
           children: <Widget>[
-            ActionButton(() {setState(() {_buttons = hazardButtons();});}, Colors.red, FontAwesomeIcons.exclamationTriangle, "Hazard"),
-            ActionButton(() {setState(() {_buttons = hazardButtons();});}, Colors.black, FontAwesomeIcons.skullCrossbones, "Crime"),
-            ActionButton(() {setState(() {_buttons = hazardButtons();});}, Colors.green, FontAwesomeIcons.toilet, "Utilities"),
+            ActionButton(() {
+              setState(() {
+                _buttons = hazardButtons();
+              });
+            }, Colors.red, FontAwesomeIcons.exclamationTriangle, "Hazard"),
+            ActionButton(() {
+              setState(() {
+                _buttons = crimeButtons();
+              });
+            }, Colors.black, FontAwesomeIcons.skullCrossbones, "Crime"),
+            ActionButton(() {
+              setState(() {
+                _buttons = utilButtons();
+              });
+            }, Colors.green, FontAwesomeIcons.lightbulb, "Utility"),
           ],
           mainAxisAlignment: MainAxisAlignment.center,
         ),
@@ -68,18 +86,48 @@ class _HomeScreenState extends State<HomeScreen> {
       children: <Widget>[
         Row(
           children: <Widget>[
-            ActionButton(() => {}, Colors.orange, FontAwesomeIcons.fireAlt, "Fire"),
-            ActionButton(() => {}, Colors.blue, FontAwesomeIcons.water, "Flood"),
-            ActionButton(() => {}, Colors.black, FontAwesomeIcons.hammer, "Infra Damage"),
+            ActionButton(
+                () => addMarker("Fire"), Colors.orange, FontAwesomeIcons.fireAlt, "Fire",
+                ),
+            ActionButton(
+                () => addMarker("Accident"), Colors.blue, FontAwesomeIcons.carCrash, "Accident"),
+            ActionButton(() =>  addMarker("Infra Damage"), Colors.black, FontAwesomeIcons.hammer,
+                "Infra Damage"),
           ],
         ),
+      ],
+    );
+  }
+
+  Widget crimeButtons() {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            ActionButton(
+                () =>  addMarker("Violent"), Colors.red, FontAwesomeIcons.dizzy, "Violent"),
+            ActionButton(
+                () =>  addMarker("Not Violent"), Colors.blue, FontAwesomeIcons.user, "Not Violent"),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget utilButtons() {
+    return Column(
+      children: <Widget>[
         Row(
           children: <Widget>[
-            ActionButton(() => {}, Colors.yellow[500], FontAwesomeIcons.bolt, "Electrical"),
-            ActionButton(() => {}, Colors.red, FontAwesomeIcons.carCrash, "Accident"),
-            ActionButton(() => {}, Colors.black, FontAwesomeIcons.hammer, "Infra Damage"),
+            ActionButton(
+                () => {}, Colors.blue, FontAwesomeIcons.restroom, "Restroom"),
+            ActionButton(
+                () => {}, Colors.red, FontAwesomeIcons.parking, "Parking"),
+            ActionButton(() => {}, Colors.green, FontAwesomeIcons.trash,
+                "Trash"),
           ],
-        )
+        ),
       ],
     );
   }
@@ -95,6 +143,29 @@ class _HomeScreenState extends State<HomeScreen> {
   IconData _buttonIcon;
   File _image;
   String _description;
+
+  Set<Marker> _markers = Set();
+  static const LatLng _center = const LatLng(0, 0);
+  LatLng _lastPosition = _center;
+
+Future addMarker(String danger) async {
+  if (danger == null) {
+    danger = "danger";
+  }
+  print("yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"); // for testing purps
+  _getLocation();
+  Marker mark = Marker(
+      position: LatLng(_latitude, _longitude),
+      markerId: MarkerId(danger),
+      infoWindow: InfoWindow(title: danger)
+      );
+
+  // setState(() {
+    _markers.add(mark);
+  // });
+
+}
+
 
   Widget _buttons;
 
@@ -117,35 +188,36 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  Future getDescriptionAndImage(BuildContext context, Color color, String tag) async {
+  Future getDescriptionAndImage(
+      BuildContext context, Color color, String tag) async {
     TextEditingController descriptionController = TextEditingController();
     await showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(tag.toUpperCase()),
-          content: TextField(
-            controller: descriptionController,
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: color)),
-              labelText: "Description",
-              labelStyle: TextStyle(color: color)
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(tag.toUpperCase()),
+            content: TextField(
+              controller: descriptionController,
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  focusedBorder:
+                      OutlineInputBorder(borderSide: BorderSide(color: color)),
+                  labelText: "Description",
+                  labelStyle: TextStyle(color: color)),
             ),
-          ),
-          actions: <Widget>[
-            MaterialButton(
-              child: Text("OK", style: TextStyle(color: color)),
-              onPressed: () => Navigator.pop(context),
-            )
-          ],
-        );
-      }
-    );
+            actions: <Widget>[
+              MaterialButton(
+                child: Text("OK", style: TextStyle(color: color)),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          );
+        });
 
-    var image = await ImagePicker.pickImage(source: ImageSource.camera, maxWidth: 450.0);
+    var image = await ImagePicker.pickImage(
+        source: ImageSource.camera, maxWidth: 450.0);
     image = await FlutterExifRotation.rotateImage(path: image.path);
 
     setState(() {
@@ -171,16 +243,16 @@ class _HomeScreenState extends State<HomeScreen> {
 
     print("Sending ${json.encode(data)}");
 
-    http.post(bobbaServer + "/api/add", body: json.encode(data))
-    .then((res) {
+    http.post(bobbaServer + "/api/add", body: json.encode(data)).then((res) {
       print(res.body);
     });
   }
 
-
   // map created
+  GoogleMapController mapController;
   void _onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
+    mapController = controller;
   }
 
   void setButtons(Widget buttons) {
@@ -201,8 +273,14 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void setArrow(String direction) {
     setState(() {
-      _buttonIcon = (direction == "up") ? Icons.keyboard_arrow_up : Icons.keyboard_arrow_down;
+      _buttonIcon = (direction == "up")
+          ? Icons.keyboard_arrow_up
+          : Icons.keyboard_arrow_down;
     });
+  }
+
+  void _onPressed() {
+    // todo: connect this to twilio
   }
 
   @override
@@ -213,12 +291,19 @@ class _HomeScreenState extends State<HomeScreen> {
       myLocationEnabled: true,
       // set init cam pos
       initialCameraPosition:
-          CameraPosition(target: LatLng(_latitude, _longitude), zoom: 14.0),
+          CameraPosition(target: LatLng(_latitude, _longitude), zoom: 17.0),
+      markers: Set.from(_markers),
     );
 
     return Scaffold(
+        // appBar: AppBar(
+        //   backgroundColor: Colors.white,
+        //   title: Text("Intellicity", style: TextStyle(color: Colors.black, fontSize: 20.0)),
+        //   centerTitle: true,
+        //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20.0), bottomRight: Radius.circular(20.0)))
+        // ),
         body: SlidingUpPanel(
-            maxHeight: MediaQuery.of(context).size.height * 0.7,
+            maxHeight: MediaQuery.of(context).size.height * 0.5,
             controller: _panelController,
             renderPanelSheet: false,
             onPanelOpened: () => setArrow("down"),
@@ -229,6 +314,7 @@ class _HomeScreenState extends State<HomeScreen> {
               });
               Vibration.vibrate(duration: 100);
             },
+
             panel: Stack(
               children: <Widget>[
                 Container(
@@ -241,28 +327,23 @@ class _HomeScreenState extends State<HomeScreen> {
                           topRight: Radius.circular(20.0))),
                 ),
                 Align(
-                  alignment: Alignment.topCenter,
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        child: IconButton(icon: Icon(_buttonIcon), onPressed: togglePanel),
-                        padding: EdgeInsets.all(20.0)
-                      ),
-                      AnimatedSwitcher(
-                        duration: const Duration(seconds: 2),
-                        child: _buttons,
-                      )
-                    ],
-                  )
-                ),
+                    alignment: Alignment.topCenter,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                            child: IconButton(
+                                icon: Icon(_buttonIcon),
+                                onPressed: togglePanel),
+                            padding: EdgeInsets.all(20.0)),
+                        AnimatedSwitcher(
+                          duration: const Duration(seconds: 2),
+                          child: _buttons,
+                        )
+                      ],
+                    )),
               ],
             ),
-        body: Container(
-          child: map,
-          height: 200,
-          width: 200
-        )
-      )
-    );
+            body: Container(child: map, height: 200, width: 200)));
+
   }
 }
