66a387ef0c5de5cc9dcf0a4027be1b772c32beea
BobbaTea <bobba.akash@gmail.com>
2019-09-08 02:53:15 -0400

    asugdfuasd

--- safetynetserver/app/app.py
+++ safetynetserver/app/app.py
@@ -1,25 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_socketio import SocketIO, emit, Namespace
+import socketio
 
 app = Flask(__name__)
 CORS(app)
-
-socketio = SocketIO(app)
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
-
-@socketio.on('message', namespace = '/socket')
-def handle_message(message):
-    print("messsage" + message)
-    emit('update', message, broadcast=True)
-
-@socketio.on('connect',namespace = '/socket')
-def test_connect():
-    print('connect')
-    # emit('my response', {'data': 'Connected'})
-
-@socketio.on('disconnect',namespace = '/socket')
-def test_disconnect():
-    print('Client disconnected')
--- safetynetserver/app/main.py
+++ safetynetserver/app/main.py
@@ -1,5 +1,23 @@
 from app import app
 import routes
+from flask_socketio import SocketIO, emit
 
-# if __name__ == "__main__":
-#     app.run(debug=True, port=5000)
+socketio = SocketIO(app)
+
+@socketio.on('message', namespace='/socket')
+def handle_message(message):
+    emit('message', message, broadcast=True)
+
+@socketio.on('connect', namespace='/socket')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/socket')
+def test_disconnect():
+    print('Client disconnected')
+
+if __name__ == "__main__":
+    # app.run(debug=True, port=5000)
+    socketio.run(app, debug=True, port=5000)
+
+
