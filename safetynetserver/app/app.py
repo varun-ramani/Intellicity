7c5a1c77befae0b64bca2bcaf1da8e5019561cc6
BobbaTea <bobba.akash@gmail.com>
2019-09-08 02:16:46 -0400

    asbdfkjasdf

--- safetynetserver/app/app.py
+++ safetynetserver/app/app.py
@@ -12,11 +12,13 @@ if __name__ == '__main__':
 
 @socketio.on('message', namespace='/socket')
 def handle_message(message):
+    print(message)
     emit('update', message, broadcast=True)
 
 @socketio.on('connect', namespace='/socket')
 def test_connect():
-    emit('my response', {'data': 'Connected'})
+    print('connect')
+    # emit('my response', {'data': 'Connected'})
 
 @socketio.on('disconnect', namespace='/socket')
 def test_disconnect():
